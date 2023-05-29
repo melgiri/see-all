c7786e5815df349abaf642bff088dfd97757e38e
melgiri <melgiri@localhost.localdomain>
2023-05-29 11:46:25 +0530

    see all - v2

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This is a small script to monitor your Services EASILY.
 
 > This will need ip and port to monitor the service
 
+>Update available.
+>1. ipv4 and ipv6 both supported
+>2. tcp and udp supported
+
 ## To install all the things here are the steps
 
 Download the zip of this project or clone using the command
@@ -11,13 +15,11 @@ Download the zip of this project or clone using the command
 git clone git@github.com:melgiritechnology/see-all.git
 ```
 
-> `socket`, `threading`, `requests` are the python packages you need to install if you dont have. You can install using these commands
-
-```
-pip install socket
-pip install threading
-pip install requests
-```
+>To install requirements (if you want discord notification) run
+>
+>```
+>pip install -r requirements.txt
+>```
 
 Go to `config.json` to config.
 
@@ -41,8 +43,6 @@ Also fill your discord user id and webhook url.
 `intervel` in the config file is the time which tells the script to check if all the services are up or not.
 
 To setup services follow the patterns followed in the `data.json` file.
-
-> Note: You can use normal ipv4. It also works with domain names.
 
 > Whenever you run the python script note that it will run constantly and show the status of the services in the console. If you stop it, it will not work. It is recommended to run it in background. Also if you put intervel to very low number it may harm your system and services.
 
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import requests
 ip = []
 port = []
 name = []
+protocol = []
+ipv = []
 
 with open("config.json") as c:
     cd = json.load(c)
@@ -16,7 +18,7 @@ user_id = cd['user_id']
 url = cd['url']
 
 data = {
-    "username" : "SEE *",
+    "username" : "SEE ALL",
     "avatar_url" : "https://png2.cleanpng.com/sh/2d60ee9fb2d434f37956b1394291ffef/L0KzQYm3UsE3N5t5fZH0aYP2gLBuTfNwdaF6jNd7LX3yfrr7jCIudZDzgeZ4cnnxd376mgN1bZ4yhdH3aYTygn7qjPlxNZJ3RdV4bYD4hLb5TgRwd50ye95ycHH1hMS0VfE5P2E5T9YEZXLpdIG1U8Y3PmU1SqQ6NUG7R4qAWcQ6PmY1S5D5bne=/kisspng-computer-monitor-monitoring-system-monitor-clip-ar-computer-tool-cliparts-5a87047d9ebfd0.3666402215187979496503.png"
 }
 
@@ -26,23 +28,54 @@ with open("data.json") as f:
         ip.append(d[i]['ip'])
         port.append(d[i]['port'])
         name.append(d[i]['name'])
+        protocol.append(d[i]['protocol'])
+        ipv.append(d[i]['ipv'])
+
+def tcp(v):
+    if v == 4:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif v == 6:
+        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    s.settimeout(5)
+    res(s)
+
+def udp(v):
+    if v == 4:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    elif v == 6:
+        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    s.settimeout(5)
+    res(s)
+
+def res(s):
+    try:
+        conn = s.connect_ex((ip[i], port[i]))
+        if not conn:
+            print(f"The device with name {name[i]} which has ip {ip[i]} and port {port[i]} is Active right now")
+        else:
+            print(f"The device with name {name[i]} which has ip {ip[i]} and port {port[i]} is InActive right now")
+            if cd['discord'] == "True":
+                data["content"] = f"<@{user_id}>\nThe device with name `{name[i]}` which has ip `{ip[i]}` and port `{port[i]}` is `InActive` right now"
+                requests.post(url, json = data)
+        s.close()
+    except socket.gaierror:
+        print(f"Someting is wrong in your data.json file with {name[i]}")
 
 def see(i):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(5)
-    target = socket.gethostbyname(ip[i])
-    conn = s.connect_ex((target, port[i]))
-    if not conn:
-        print(f"The device with name {name[i]} which has ip {target} and port {port[i]} is Active right now")
-    else:
-        print(f"The device with name {name[i]} which has ip {target} and port {port[i]} is InActive right now")
-        if cd['discord'] == "True":
-            data["content"] = f"<@{user_id}>\nThe device with name `{name[i]}` which has ip `{target}` and port `{port[i]}` is `InActive` right now"
-            requests.post(url, json = data)
+    if protocol[i] == "tcp" or protocol != "udp":
+        if ipv[i] == "4" or ipv[i] != "6":
+            tcp(4)
+        elif ipv[i] == "6":
+            tcp(6)
+    elif protocol[i] == "udp":
+        if ipv[i] == "4" or ipv[i] != "6":
+            udp(4)
+        elif ipv[i] == "6":
+            udp(6)
 
 while True:
     for i in range(0, len(ip)):
         thread = threading.Thread(target=see, args=(i,), daemon=True).start()
         time.sleep(0.01)
     time.sleep(cd['intervel'])
-    os.system("clear")
+    print("-"*80)
